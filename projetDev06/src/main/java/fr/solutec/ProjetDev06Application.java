dfd7296b15bdda4536d7ebe748b00056964a363b
y.morvan <y.morvan@I2021--9633.estia.local>
2021-12-16 16:13:47 +0100

    TO

---
+++ projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDev06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDev06Application.class, args);
+	}
+
+}
---
+++ projetDev06/src/test/java/fr/solutec/ProjetDev06ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDev06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
