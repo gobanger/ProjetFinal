eda781721a167a94e2031079a9bc28e6e7cb7b6e
Maxime <Maxime@DESKTOP-0NI63GE>
2021-12-18 12:58:41 +0100

    MAJ main & Application properties

--- projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
+++ projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
@@ -1,16 +1,20 @@
 package fr.solutec;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class ProjetDev06Application {
+public class ProjetDev06Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetDev06Application.class, args);
-		
-		// Salut
-		// comment tu vas nicolas ?
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("lancement projet");
 	}
 
 }
