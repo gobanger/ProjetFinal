36811de1e3dc17c25ef64e57a39dc1cac2dd4016
Nicolas <burtznicolas@yahoo.fr>
2021-12-20 12:32:04 +0100

    v

--- projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
+++ projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import fr.solutec.entities.Candidat;
 import fr.solutec.entities.Connexion;
 import fr.solutec.entities.Inscription;
@@ -33,19 +32,19 @@ public class ProjetDev06Application implements CommandLineRunner {
 		System.out.println("lancement projet OK");
 
 		System.out.println(ZonedDateTime.now());
-	/*
-		Candidat c1 = new Candidat(null, "chameau@mail.fr", "lama", "Chameau", "Lama", true);
+	
+		Candidat c1 = new Candidat(null, null, "chameau@mail.fr", "lama", "Chameau", "Lama", null, null, null, true, 0);
 		candiRepo.save(c1);
-		Candidat c2 = new Candidat(null, "league@mail.fr", "lol", "Yasuo", "Garen", false);
+		Candidat c2 = new Candidat(null, null, "league@mail.fr", "lol", "Yasuo", "Garen", null, null, null, false, 0);
 		candiRepo.save(c2);
-	*/
+	
 		Inscription i1 = new Inscription(null,"nicolas@yahoo.fr","james");
 		inscriRepo.save(i1);
 		System.out.println("i1");
 		
-		Connexion c1 = new Connexion(null,"burtz","nicolas", "nicolas@yahoo.fr", "11", "11");
-		connexionRepo.save(c1);
-		System.out.println("c1");
+		Connexion co1 = new Connexion(null,"burtz","nicolas", "nicolas@yahoo.fr", "11", "11");
+		connexionRepo.save(co1);
+		System.out.println("co1");
 	}
 	
 }
