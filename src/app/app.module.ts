ffa287f99c78a0ce39e81e287d1c51e42c7a1dd6
GwendolineGrollier <gwendoline.grollier@live.fr>
2021-12-20 11:03:54 +0100

    AccueilV2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
