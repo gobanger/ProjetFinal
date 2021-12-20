f6621068bc1d998354f41525cdaedc413062811d
Nicolas <burtznicolas@yahoo.fr>
2021-12-20 14:57:32 +0100

    ghvh

--- projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
+++ projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
@@ -31,20 +31,25 @@ public class ProjetDev06Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("lancement projet OK");
 
-		System.out.println(ZonedDateTime.now());
+//		System.out.println(ZonedDateTime.now());
 	
 		Candidat c1 = new Candidat(null, null, "chameau@mail.fr", "lama", "Chameau", "Lama", null, null, null, true, 0);
 		candiRepo.save(c1);
+//		System.out.println(c1);
 		Candidat c2 = new Candidat(null, null, "league@mail.fr", "lol", "Yasuo", "Garen", null, null, null, false, 0);
 		candiRepo.save(c2);
+//		System.out.println(c2);
 	
 		Inscription i1 = new Inscription(null,"nicolas@yahoo.fr","james");
-		inscriRepo.save(i1);
-		System.out.println("i1");
+		inscriRepo.newUser(i1);
+	
+	
 		
-		Connexion co1 = new Connexion(null,"burtz","nicolas", "nicolas@yahoo.fr", "11", "11");
+		System.out.println(i1);
+		
+		/* Connexion co1 = new Connexion(null,"burtz","nicolas", "nicolas@yahoo.fr", "11", "11");
 		connexionRepo.save(co1);
-		System.out.println("co1");
+		System.out.println(co1); */
 	}
 	
 }
