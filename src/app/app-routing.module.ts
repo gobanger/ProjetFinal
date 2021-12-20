16c89f723f1092127ceac938c0b1c03fb56cd85b
gobanger <uranium.droite@gmail.com>
2021-12-20 12:36:38 +0100

    index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path : 'accueil' , component : AccueilComponent}
+  {path : 'accueil' , component : AccueilComponent},
+  {path : 'home' , component : HomeComponent}
 ];
 
 @NgModule({
