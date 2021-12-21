8584e2e8cb008ed841c346603724e7448d71f7c7
Nicolas <burtznicolas@yahoo.fr>
2021-12-21 09:51:38 +0100

    9:51 ok

--- projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
+++ projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
@@ -1,47 +1,26 @@
 package fr.solutec;
 
-<<<<<<< HEAD
-
 import java.time.ZonedDateTime;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-=======
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-import fr.solutec.entities.Candidat;
-import fr.solutec.entities.Connexion;
-import fr.solutec.entities.Inscription;
-=======
-
-
 import fr.solutec.entities.Candidat;
 import fr.solutec.entities.Conseiller;
-import fr.solutec.entities.Rdv;
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
 import fr.solutec.repository.CandidatRepository;
-<<<<<<< HEAD
 import fr.solutec.repository.ConnexionRepository;
-import fr.solutec.repository.InscriptionRepository;
-=======
 import fr.solutec.repository.ConseillerRepository;
 import fr.solutec.repository.RdvRepository;
 
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
-
 @SpringBootApplication
 public class ProjetDev06Application implements CommandLineRunner {
-<<<<<<< HEAD
+
 	@Autowired
 	private CandidatRepository candiRepo;
 	@Autowired
 	private ConnexionRepository connexionRepo;
-=======
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
 	@Autowired
 	RdvRepository rdv;
 	@Autowired
@@ -56,7 +35,7 @@ public class ProjetDev06Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("lancement projet OK");
-<<<<<<< HEAD
+
 
 /*		System.out.println(ZonedDateTime.now());
 	
@@ -69,10 +48,6 @@ public class ProjetDev06Application implements CommandLineRunner {
 	
 		Inscription i1 = new Inscription(null,"nicolas@yahoo.fr","james");
 		inscriRepo.save(i1);
-	
-	
-		
-	
 		
 		Connexion co1 = new Connexion(null,"burtz","nicolas", "nicolas@yahoo.fr", "11", "11");
 		connexionRepo.save(co1);
@@ -81,7 +56,6 @@ public class ProjetDev06Application implements CommandLineRunner {
 		System.out.println(ZonedDateTime.now());
 
 		DateFormat d = new SimpleDateFormat("dd/MM/yyyy"); 
-=======
 		Candidat cd1 = new Candidat(null, "league@mail.fr", "lol", "Yasuo", "Garen", null);
 		candidatRepo.save(cd1);
 		Candidat cd2 = new Candidat(null, "juliette.nelm@gmail.com", "juju", "Nami", "Jinx", null);
@@ -90,20 +64,15 @@ public class ProjetDev06Application implements CommandLineRunner {
 		conRepo.save(cs1);
 		Conseiller cs2 = new Conseiller(null, "juliette.pims@gmail.com", "pims", "Kindred", "Zyra");
 		conRepo.save(cs2);
-		Rdv r1 = new Rdv(null, new Date(), "RDV administratif", cs1, cd1, "rdv@teams.fr", "Message additionel");
-		rdv.save(r1);
-		Rdv r2 = new Rdv(null, new Date(), "Entretien test", cs2, cd2, "rdv@teams.fr", "Préparez-vous bien");
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
-	//	System.out.println(ZonedDateTime.now());
-		
-		//Candidat c1 = new Candidat(null,d.parse("14/07/2018"));
-		//candidatRepo.save(c1);
-<<<<<<< HEAD
+//		Rdv r1 = new Rdv(null, new Date(), "RDV administratif", cs1, null, null, cs2, cd1, "rdv@teams.fr", "Message additionel");
+//		rdv.save(r1);
+//		Rdv r2 = new Rdv(null, new Date(), "Entretien test", cs2, null, null, cs2, cd2, "rdv@teams.fr", "Préparez-vous bien");
+//		System.out.println(ZonedDateTime.now());		
+//		Candidat c1 = new Candidat(null,d.parse("14/07/2018"));
+//		candidatRepo.save(c1);	
 
-	}}
-=======
+	}
 		
 	}
 
-}
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
+
