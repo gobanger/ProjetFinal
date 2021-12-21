b4fc11cdd7c841c1dbcd96fbf8f29882aa82ef75
Nicolas <burtznicolas@yahoo.fr>
2021-12-21 12:17:57 +0100

    réunification

--- projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
+++ projetDev06/src/main/java/fr/solutec/ProjetDev06Application.java
@@ -35,8 +35,6 @@ public class ProjetDev06Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("lancement projet OK");
-<<<<<<< HEAD
-
 
 /*		System.out.println(ZonedDateTime.now());
 	
@@ -57,9 +55,8 @@ public class ProjetDev06Application implements CommandLineRunner {
 		System.out.println(ZonedDateTime.now());
 
 		DateFormat d = new SimpleDateFormat("dd/MM/yyyy"); 
-=======
+	}}
 		/*
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
 		Candidat cd1 = new Candidat(null, "league@mail.fr", "lol", "Yasuo", "Garen", null);
 		candidatRepo.save(cd1);
 		Candidat cd2 = new Candidat(null, "juliette.nelm@gmail.com", "juju", "Nami", "Jinx", null);
@@ -68,7 +65,6 @@ public class ProjetDev06Application implements CommandLineRunner {
 		conRepo.save(cs1);
 		Conseiller cs2 = new Conseiller(null, "juliette.pims@gmail.com", "pims", "Kindred", "Zyra");
 		conRepo.save(cs2);
-<<<<<<< HEAD
 //		Rdv r1 = new Rdv(null, new Date(), "RDV administratif", cs1, null, null, cs2, cd1, "rdv@teams.fr", "Message additionel");
 //		rdv.save(r1);
 //		Rdv r2 = new Rdv(null, new Date(), "Entretien test", cs2, null, null, cs2, cd2, "rdv@teams.fr", "Préparez-vous bien");
@@ -77,14 +73,11 @@ public class ProjetDev06Application implements CommandLineRunner {
 //		candidatRepo.save(c1);	
 
 	}
-=======
 		Rdv r1 = new Rdv(null, new Date(), "RDV administratif", cs1, cd1, "rdv@teams.fr", "Message additionel");
 		rdv.save(r1);
 		Rdv r2 = new Rdv(null, new Date(), "Entretien test", cs2, cd2, "rdv@teams.fr", "Préparez-vous bien");
 		*/
 
->>>>>>> branch 'master' of https://github.com/gobanger/ProjetFinal.git
-		
-	}
+
 
 
