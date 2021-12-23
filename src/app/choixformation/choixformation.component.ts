645692c96ab48717907b5476922df9c54e5f2e5b
GwendolineGrollier <gwendoline.grollier@live.fr>
2021-12-23 16:17:26 +0100

    Gwen21

---
+++ src/app/choixformation/choixformation.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-choixformation',
+  templateUrl: './choixformation.component.html',
+  styleUrls: ['./choixformation.component.css']
+})
+export class ChoixformationComponent implements OnInit {
+  information : any;
+  msgValidation: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+
+  choixformation(information:any): any{
+    this.http.post('http://localhost:8086/choixformation',information).subscribe({
+      next:(data) => {
+        information = data;
+        console.log(information);
+        this.msgValidation="inscription complete";
+      },
+      error:(err) => {
+        console.log(information);
+      },
+
+
+    })
+  }
+
+}
